7416549ae83f9ff96647b840f7d4fa4b0fad95a3
Mohamed Saber <msaber9765@gmail.com>
2022-09-19 17:29:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        TeatFrame testFrame = new TeatFrame();
+        testFrame.setVisible(true);
+
+    }
+}
---
+++ src/TeatFrame.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class TeatFrame extends JFrame implements ActionListener {
+
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem loadFile, saveFile, exit;
+    JTable table;
+    JTextArea textArea;
+
+    public TeatFrame() {
+        super("Test Frame");
+
+        setSize(700, 1000);
+        setLocation(100, 100);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        loadFile = new JMenuItem("Load File", 'L');
+        loadFile.addActionListener(this);
+        loadFile.setActionCommand("l");
+        saveFile = new JMenuItem("Save File", 'S');
+        saveFile.addActionListener(this);
+        saveFile.setActionCommand("s");
+        exit = new JMenuItem("Exit", 'E');
+        exit.addActionListener(this);
+        exit.setActionCommand("e");
+        menu.add(loadFile);
+        menu.add(saveFile);
+        menu.addSeparator();
+        menu.add(exit);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+        String[] cols = {"No.", "Date", "Customer", "Total"};
+        int rows = 4;
+        DefaultTableModel model = new DefaultTableModel(rows, cols.length);
+        model.setColumnIdentifiers(cols);
+        table = new JTable(model);
+        add(new JScrollPane(table));
+
+        textArea = new JTextArea();
+        add(new JScrollPane(textArea));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "l" -> openFile();
+            case "s" -> saveFile();
+            case "e" -> System.exit(0);
+        }
+    }
+
+    void openFile() {
+        JFileChooser fs = new JFileChooser();
+        int result = fs.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            String path = fs.getSelectedFile().getPath();
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(path);
+                int size = fis.available();
+                byte[] b = new byte[size];
+                fis.read(b);
+                textArea.setText(new String(b));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    void saveFile() {
+        JFileChooser fs2 = new JFileChooser();
+        int result = fs2.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            String path = fs2.getSelectedFile().getPath();
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(path);
+                byte[] b = textArea.getText().getBytes();
+                fos.write(b);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
